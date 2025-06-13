f178583b6a89a8468d729e5d3a8699b3a488d65b
Max <max05gonz@gmail.com>
2025-06-13 08:14:17 -0500

    Se agregaron varios botones en la página de alerta

--- src/app/pagina/alert/alert.page.ts
+++ src/app/pagina/alert/alert.page.ts
@@ -3,29 +3,73 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { ComponentesModule } from 'src/app/componentes/componentes.module';
-import { AlertController, IonButton } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular/standalone';
+import { IonAlert, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
   styleUrls: ['./alert.page.scss'],
   imports: [IonicModule, CommonModule, RouterModule, ComponentesModule],
+  
 })
+
 export class AlertPage implements OnInit {
-
-  constructor(private CtrlAlert: AlertController) { }
-
+  constructor(private alertController: AlertController) {}
+       
   ngOnInit() {
   }
-
-  async funcionalerta1() {
-    const alert = await this.CtrlAlert.create({
+  async funcionAlerta1() {
+    const alert = await this.alertController.create({
       header: 'A Short Title Is Best',
       subHeader: 'A Sub Header Is Optional',
       message: 'A message should be a short, complete sentence.',
       buttons: ['Action'],
+
+    
     });
 
+    
     await alert.present();
   }
+
+  public alertButtons = ['OK'];
+  public alertInputs = [
+    {
+      placeholder: 'Nombre',
+    },
+    {
+      placeholder: 'Apellido Paterno',
+      attributes: {
+        maxlength: 20,
+      },
+    },
+    {
+      placeholder: 'Apellido Materno',
+      attributes: {
+        maxlength: 20,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'Edad',
+      min: 1,
+      max: 100,
+    },
+  ];
+
+  async funcionCosole() {
+    const alert = await this.alertController.create({
+      header: 'Mensaje Consola',
+      subHeader: '',
+      message: '',
+      buttons: ['Action'],
+
+    
+    });
+
+    
+    await alert.present();
+  }
+  
 }
